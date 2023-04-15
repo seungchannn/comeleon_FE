e1ad91dbab4ce86c7b4dcbae6e42366946586b3d
HeinLee <heinlee4680@gmail.com>
2023-04-15 17:29:51 +0900

    setting완료 : : ;

---
+++ src/components/Login/Login.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import * as S from './Login.style';
+
+const Login = () => {
+  return (
+    <S.LoginContainer>
+      <p>dddd</p>
+    </S.LoginContainer>
+  );
+};
+
+export default Login;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import * as ReactDOM from 'react-dom/client';
 import Router from './Router';
-import Globalstyle from './Styles/Globalstyle';
+import GlobalStyle from './styles/GlobalStyle';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <>
-    <Globalstyle />
+    <GlobalStyle />
     <Router />
   </>
 );
