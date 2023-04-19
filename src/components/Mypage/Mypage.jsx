af3c79d25c32706d3f999e1fd98b40398769ad48
HeinLee <heinlee4680@gmail.com>
2023-04-19 18:52:26 +0900

    mypage구현중

---
+++ src/components/Mypage/Mypage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import * as S from './Mypage.style';
+
+const Mypage = () => {
+  return (
+    <S.MypageWhole>
+      <div>하하하</div>
+    </S.MypageWhole>
+  );
+};
+export default Mypage;
