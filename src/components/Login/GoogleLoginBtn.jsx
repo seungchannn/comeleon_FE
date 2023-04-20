cc18d882d4b7a0b7da4cc1c25d81f428ff9698e1
HeinLee <heinlee4680@gmail.com>
2023-04-20 10:18:38 +0900

    로그인통신시도중

--- src/components/Login/GoogleLoginBtn.jsx
+++ src/components/Login/GoogleLoginBtn.jsx
@@ -1,33 +1,91 @@
+// import React from 'react';
+// import {
+//   GoogleLogin,
+//   GoogleOAuthProvider,
+//   useGoogleLogin,
+// } from '@react-oauth/google';
+// import jwt_decode from 'jwt-decode';
+// import * as S from './GoogleLoginBtn.style';
+
+// const GoogleLoginBtn = () => {
+//   const clientId = process.env.REACT_APP_GOOGLE_CLIENT_ID;
+//   // const login = useGoogleLogin({
+//   //   onSuccess: credentialResponse => {
+//   //     console.log('로그인성공!', credentialResponse);
+
+//   //     const decodeding = jwt_decode(credentialResponse.credential);
+//   //     console.log(decodeding);
+//   //   },
+//   // });
+
+//   return (
+//     <GoogleOAuthProvider clientId={clientId}>
+//     <GoogleLogin
+//       type="standard"
+//       onSuccess={handleLoginSuccess}
+//       theme="filled_blue" //파란색 하고싶은경우 적용하기
+//         shape="circle"
+//         text="signin_with"
+//         size="medium"
+//         cancel_on_tap_outside="true"
+//       onFailure={(err) => {
+//         console.log('로그인실패', err);
+//       }}
+//     />
+//   </GoogleOAuthProvider>
+//     // <GoogleOAuthProvider clientId={clientId}>
+//     //   <GoogleLogin
+//     //     type="standard"
+//     //     onSuccess={credentialResponse => {
+//     //       console.log('로그인성공!', credentialResponse);
+//     //       // const decodeding = jwt_decode(credentialResponse.credential);
+//     //       // console.log(decodeding);
+//     //     }}
+//     //     theme="filled_blue" //파란색 하고싶은경우 적용하기
+//     //     shape="circle"
+//     //     text="signin_with"
+//     //     size="medium"
+//     //     cancel_on_tap_outside="true"
+//     //     onFailure={err => {
+//     //       console.log('로그인실패', err);
+//     //     }}
+//     //   />
+//     // </GoogleOAuthProvider>
+//   );
+// };
+// export default GoogleLoginBtn;
+
 import React from 'react';
-import {
-  GoogleLogin,
-  GoogleOAuthProvider,
-  useGoogleLogin,
-} from '@react-oauth/google';
-import jwt_decode from 'jwt-decode';
-import * as S from './GoogleLoginBtn.style';
+import { GoogleLogin, GoogleOAuthProvider } from '@react-oauth/google';
 
 const GoogleLoginBtn = () => {
   const clientId = process.env.REACT_APP_GOOGLE_CLIENT_ID;
-  // const login = useGoogleLogin({
-  //   onSuccess: credentialResponse => {
-  //     console.log('로그인성공!', credentialResponse);
 
-  //     const decodeding = jwt_decode(credentialResponse.credential);
-  //     console.log(decodeding);
-  //   },
-  // });
+  const handleLoginSuccess = async credentialResponse => {
+    console.log('로그인성공!', credentialResponse);
+
+    // credentialResponse 객체를 그대로 전송
+    const response = await fetch('http://172.30.1.41:3310/users/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(credentialResponse),
+    });
+
+    if (response.ok) {
+      console.log('성공');
+    } else {
+      console.log('실패');
+    }
+  };
 
   return (
     <GoogleOAuthProvider clientId={clientId}>
       <GoogleLogin
         type="standard"
-        onSuccess={credentialResponse => {
-          console.log('로그인성공!', credentialResponse);
-          // const decodeding = jwt_decode(credentialResponse.credential);
-          // console.log(decodeding);
-        }}
-        theme="filled_blue" //파란색 하고싶은경우 적용하기
+        onSuccess={handleLoginSuccess}
+        theme="filled_blue"
         shape="circle"
         text="signin_with"
         size="medium"
@@ -39,21 +97,5 @@ const GoogleLoginBtn = () => {
     </GoogleOAuthProvider>
   );
 };
+
 export default GoogleLoginBtn;
-
-//AccessToken 받기원하면 이거 이용
-// function App() {
-//   const login = useGoogleLogin({
-//     onSuccess: codeResponse => console.log(codeResponse),
-//   });
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         {/* This is my button. Its just a regular HTML Button */}
-//         <button onClick={() => login()}>Log In Using Google</button>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
