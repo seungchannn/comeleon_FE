7c2a78e2e84dd5f16065b0c81f2a257eeeb8f495
HeinLee <heinlee4680@gmail.com>
2023-04-20 18:54:08 +0900

    add:profileUpload

--- src/components/Mypage/Mypage.jsx
+++ src/components/Mypage/Mypage.jsx
@@ -2,6 +2,13 @@ import React, { useState } from 'react';
 import * as S from './Mypage.style';
 
 const Mypage = () => {
+  const [uploadedImage, setUploadedImage] = useState(null);
+
+  const onChangeImage = e => {
+    const file = e.target.files[0];
+    const imageUrl = URL.createObjectURL(file);
+    setUploadedImage(imageUrl);
+  };
   return (
     <S.MypageWhole>
       <div>
@@ -10,8 +17,12 @@ const Mypage = () => {
           <img src="./images/logout.png" alt="로그아웃" />
         </div>
         <div>
-          <img src="./images/profile.png" alt="프로필사진" />
-          <input type="file" />
+          {uploadedImage ? (
+            <S.MyProfileImg src={uploadedImage} alt="프로필 없을때" />
+          ) : (
+            <S.MyProfileImg src="./images/profile.png" alt="프로필사진" />
+          )}
+          <input type="file" onChange={onChangeImage} />
         </div>
       </div>
     </S.MypageWhole>
--- src/components/Mypage/Mypage.style.js
+++ src/components/Mypage/Mypage.style.js
@@ -4,3 +4,17 @@ export const MypageWhole = styled.div`
   width: 100vw;
   height: 100&;
 `;
+
+// export const MyProfileBox = styled.div`
+//   width: 200px;
+//   height: 200px;
+//   border-radius: 100%;
+//   background-color: gray;
+// `;
+
+export const MyProfileImg = styled.img`
+  width: 170px;
+  height: 170px;
+  border-radius: 100%;
+  border: 7px solid #f5f5fb;
+`;
