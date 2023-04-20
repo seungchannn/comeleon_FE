da97ce92b809b975988308b943cf10384e6a4e3e
HeinLee <heinlee4680@gmail.com>
2023-04-20 18:24:06 +0900

    계속수정중

--- src/components/Login/GoogleLoginBtn.jsx
+++ src/components/Login/GoogleLoginBtn.jsx
@@ -61,32 +61,14 @@ import { GoogleLogin, GoogleOAuthProvider } from '@react-oauth/google';
 const GoogleLoginBtn = () => {
   const clientId = process.env.REACT_APP_GOOGLE_CLIENT_ID;
 
-  const handleLoginSuccess = async credentialResponse => {
+  const handleLoginSuccess = credentialResponse => {
     console.log('로그인성공!', credentialResponse);
-    await postCredential(credentialResponse);
+    postCredential(credentialResponse);
   };
 
-  // credentialResponse 객체를 그대로 전송
-  // const response = await fetch('http://172.30.1.41:3310/users/login', {
-  //   method: 'POST',
-  //   headers: {
-  //     'Content-Type': 'application/json',
-  //   },
-  //   body: JSON.stringify(credentialResponse),
-  // });
-
-  // console.log('로그인후 응답', response);
-
-  // if (response.ok) {
-  //   const responseText = await response.text();
-  //   console.log('응답?', responseText);
-  //   console.log('성공');
-  // } else {
-  //   console.log('실패');
-  // }
-
-  const postCredential = async credentialResponse => {
-    const response = await fetch('http://172.30.1.41:3000/users/login', {
+  const postCredential = credentialResponse => {
+    console.log('dddd', credentialResponse);
+    fetch('http://172.30.1.41:3000/users/login', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
@@ -94,15 +76,15 @@ const GoogleLoginBtn = () => {
       body: JSON.stringify(credentialResponse),
     });
 
-    console.log('로그인후 응답', response);
+    // console.log('로그인후 응답', response);
 
-    if (response.ok) {
-      const responseText = await response.text();
-      console.log('응답?', responseText);
-      console.log('성공');
-    } else {
-      console.log('실패');
-    }
+    // if (response.ok) {
+    //   const responseText = response.text();
+    //   console.log('응답?', responseText);
+    //   console.log('성공');
+    // } else {
+    //   console.log('실패');
+    // }
   };
 
   return (
@@ -124,3 +106,55 @@ const GoogleLoginBtn = () => {
 };
 
 export default GoogleLoginBtn;
+
+//원래코드
+// import React, { useEffect } from 'react';
+// import { GoogleLogin, GoogleOAuthProvider } from '@react-oauth/google';
+
+// const GoogleLoginBtn = () => {
+//   const clientId = process.env.REACT_APP_GOOGLE_CLIENT_ID;
+
+//   const handleLoginSuccess = async credentialResponse => {
+//     if (credentialResponse) {
+//       postCredential(credentialResponse);
+//       console.log('로그인성공!담았다!!', credentialResponse);
+//     } else {
+//       console.log('로그인실패!!!');
+//     }
+//   };
+
+//   const postCredential = async credentialResponse => {
+//     const response = await fetch('http://172.30.1.41:3310/users/login', {
+//       method: 'POST',
+//       headers: {켜
+//         'Content-Type': 'application/json',
+//       },
+//       body: JSON.stringify(credentialResponse),
+//     });
+
+//     if (response.ok) {
+//       console.log('자격 증명 전송 성공');
+//     } else {
+//       console.log('자격 증명 전송 실패');
+//     }
+//   };
+
+//   return (
+//     <GoogleOAuthProvider clientId={clientId}>
+//       <GoogleLogin
+//         type="standard"
+//         onSuccess={handleLoginSuccess}
+//         theme="filled_blue"
+//         shape="circle"
+//         text="signin_with"
+//         size="medium"
+//         cancel_on_tap_outside="true"
+//         onFailure={err => {
+//           console.log('로그인실패', err);
+//         }}
+//       />
+//     </GoogleOAuthProvider>
+//   );
+// };
+
+// export default GoogleLoginBtn;
