5bc0f7e6c552a18f58b91543d47ea4fa7b1cb6a2
HeinLee <heinlee4680@gmail.com>
2023-04-21 19:38:56 +0900

    add:되돌렸는데 얘네는 변경됨

---
+++ src/components/Mypage/Mypage.js
@@ -0,0 +1,7 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import * as S from './MyPage.style';
+
+export default function Mypage() {
+  return <S.MypageContainer />;
+}
