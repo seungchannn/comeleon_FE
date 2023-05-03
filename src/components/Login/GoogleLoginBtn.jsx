e23d69a4aa9817780b142a4c4ceadce6ebe404bb
HeinLee <heinlee4680@gmail.com>
2023-05-03 20:15:31 +0900

    login

--- src/components/Login/GoogleLoginBtn.jsx
+++ src/components/Login/GoogleLoginBtn.jsx
@@ -69,11 +69,14 @@ const GoogleLoginBtn = () => {
   };
   const navigate = useNavigate('/');
 
+  const token = localStorage.getItem('token');
+
   const postCredential = credentialResponse => {
     fetch('http://172.30.1.41:3310/users/login', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
+        Authorization: token,
       },
       body: JSON.stringify(credentialResponse),
       credentials: 'include',
@@ -86,12 +89,8 @@ const GoogleLoginBtn = () => {
     <GoogleOAuthProvider clientId={clientId}>
       <GoogleLogin
         type="standard"
-        onSuccess={credentialResponse => {
-          console.log('로그인성공!', credentialResponse);
-          // const decodeding = jwt_decode(credentialResponse.credential);
-          // console.log(decodeding);
-        }}
-        theme="filled_blue" //파란색 하고싶은경우 적용하기
+        onSuccess={handleLoginSuccess}
+        theme="filled_blue"
         shape="circle"
         text="signin_with"
         size="medium"
@@ -105,19 +104,3 @@ const GoogleLoginBtn = () => {
 };
 
 export default GoogleLoginBtn;
-{
-  /* <GoogleOAuthProvider clientId={clientId}>
-<GoogleLogin
-  type="standard"
-  onSuccess={handleLoginSuccess}
-  theme="filled_blue"
-  shape="circle"
-  text="signin_with"
-  size="medium"
-  cancel_on_tap_outside="true"
-  onFailure={err => {
-    console.log('로그인실패', err);
-  }}
-/>
-</GoogleOAuthProvider> */
-}
