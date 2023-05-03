4a7844786c8446c39d06bd306fb6d4175a8ff357
HeinLee <heinlee4680@gmail.com>
2023-05-03 21:22:21 +0900

    주석삭제

--- src/components/Login/GoogleLoginBtn.jsx
+++ src/components/Login/GoogleLoginBtn.jsx
@@ -1,60 +1,3 @@
-// import React from 'react';
-// import {
-//   GoogleLogin,
-//   GoogleOAuthProvider,
-//   useGoogleLogin,
-// } from '@react-oauth/google';
-// import jwt_decode from 'jwt-decode';
-// import * as S from './GoogleLoginBtn.style';
-
-// const GoogleLoginBtn = () => {
-//   const clientId = process.env.REACT_APP_GOOGLE_CLIENT_ID;
-//   // const login = useGoogleLogin({
-//   //   onSuccess: credentialResponse => {
-//   //     console.log('로그인성공!', credentialResponse);
-
-//   //     const decodeding = jwt_decode(credentialResponse.credential);
-//   //     console.log(decodeding);
-//   //   },
-//   // });
-
-//   return (
-//     <GoogleOAuthProvider clientId={clientId}>
-//     <GoogleLogin
-//       type="standard"
-//       onSuccess={handleLoginSuccess}
-//       theme="filled_blue" //파란색 하고싶은경우 적용하기
-//         shape="circle"
-//         text="signin_with"
-//         size="medium"
-//         cancel_on_tap_outside="true"
-//       onFailure={(err) => {
-//         console.log('로그인실패', err);
-//       }}
-//     />
-//   </GoogleOAuthProvider>
-//     // <GoogleOAuthProvider clientId={clientId}>
-//     //   <GoogleLogin
-//     //     type="standard"
-//     //     onSuccess={credentialResponse => {
-//     //       console.log('로그인성공!', credentialResponse);
-//     //       // const decodeding = jwt_decode(credentialResponse.credential);
-//     //       // console.log(decodeding);
-//     //     }}
-//     //     theme="filled_blue" //파란색 하고싶은경우 적용하기
-//     //     shape="circle"
-//     //     text="signin_with"
-//     //     size="medium"
-//     //     cancel_on_tap_outside="true"
-//     //     onFailure={err => {
-//     //       console.log('로그인실패', err);
-//     //     }}
-//     //   />
-//     // </GoogleOAuthProvider>
-//   );
-// };
-// export default GoogleLoginBtn;
-
 import React, { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { GoogleLogin, GoogleOAuthProvider } from '@react-oauth/google';
@@ -71,7 +14,6 @@ const GoogleLoginBtn = ({ setLoginModalOn }) => {
   };
 
   const token = localStorage.getItem('token');
-  // const saveToken = token => localStorage.setItem('token', token);
 
   const postCredential = credentialResponse => {
     fetch('http://192.168.1.127:3310/users/login', {
